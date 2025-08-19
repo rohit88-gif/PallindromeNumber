2c9bede63d5fce6577f81d78169f4baa877c2c5e
rohit88-gif <rohit1416shinde@gmail.com>
2025-08-19 13:45:35 +0530

    Add files via upload

---
+++ Program3.c
@@ -0,0 +1,21 @@
+int main()
+{
+	int num, reversed num, orignal num,reminder;
+	
+	printf("enter your number ");
+	scanf("%d"&num);
+  while(num=0)
+  {
+  	reminder=num%10;
+  	reversedNum = reversedNum * 10 + remainder;
+  	num/=10;
+  }
+  if(origanlNum=reversedNum)
+  {
+  	printf("it is palindrom number");
+  }
+  else 
+  {
+  	printf("it is not palindrom number");
+  }
+}
